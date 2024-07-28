26dbca5bbf6e3a2069791267b00770b6e85f7eb1
umanghere <umangbansal1st@gmail.com>
2024-07-28 15:27:26 +0530

    Updated Cards.jsx. There were some problems in MUI components and hence removed them

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,7 +6,6 @@ import PieChart from '../../Elements/PieChart/PieChart'
 const Dashboard = () => {
   return (
     <div>
-
     <Cards />
     <div className='flex w-full justify-center px-32 gap-32'>
       <div className='my-14 w-1/2'>
